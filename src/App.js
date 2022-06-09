e5cd33ce6afef28f9444cca00137f5838f7def0f
miaklwalker <ouisihai@gmail.com>
2022-06-09 13:28:21 -0500

    added all logic for the backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useEffect, useState} from "react";
+
+const MapContainer = (({data:[mapName,portNumber]})=>{
+  return (<div className={"map-container"}>
+            <input type={"text"} value={mapName}/>
+            <input type={"text"} value={portNumber} />
+            <button>X</button>
+          </div>)
+})
+
+function ArkAlarmForm({data}){
+  let maps = data.data
+  return (
+      <form action="">
+        <label htmlFor="">
+          server
+          <input type="text" placeholder={data.server}/>
+        </label>
+        <label htmlFor="">
+          ip
+          <input type="text" placeholder={data.ip}/>
+        </label>
+        <label htmlFor="">
+          game
+          <input type="text" placeholder={data.game}/>
+        </label>
+        <label htmlFor="">
+          maps
+          <input type="text" placeholder={""}/>
+          {Object.entries(data.maps).map((el,i)=> <MapContainer i={i} data={el}/>)}
+        </label>
+        <label htmlFor="">
+          enemies
+          <input type="text"/>
+        </label>
+        <label htmlFor="">
+          tribemates
+          <input type="text"/>
+        </label>
+      </form>
+  )
+}
+
 
 function App() {
+  const [clientData, setClientData] = useState(false);
+  useEffect(()=>{
+    let search = window.location.search.substring(1);
+    let hasSessionStorage = sessionStorage.getItem(search);
+    if(hasSessionStorage){
+      setClientData(JSON.parse(hasSessionStorage))
+    }else{
+      fetch(`/${search}`)
+          .then((res)=>res.json())
+          .then(res=>{sessionStorage.setItem(search,JSON.stringify(res))})
+          .then(()=>setClientData(JSON.parse(sessionStorage.getItem(search))))
+    }
+
+  },[])
+  let cluster = clientData && Object.keys(clientData.data);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ark Alarm</h1>
+      <h2>{clientData.name}'s config page</h2>
+      {cluster && cluster.map((name,i) => <div key={i}>{name}</div>)}
+      {cluster && cluster.map((name,i) => <ArkAlarmForm key={i} data={clientData.data[name]} /> )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,125 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createServer} from "miragejs";
+
+let database = {
+    keys: {
+        423334: {name: "Ark Alarm Test Server"}
+    },
+    serverData: {
+        "Ark": {
+            "BeardedGamers": {
+                "server": "Bearded Gamer",
+                "ip": "24.128.67.254",
+                "game": "arkse",
+                "maps": {
+                    "The Island": "27019",
+                    "Aberation": "27023",
+                    "The Center": "27017",
+                    "Ragnorak": "27015",
+                    "Valguero": "27021",
+                    "Crystal Isle": "27033",
+                    "Extinction": "27025",
+                    "Genesis": "27029",
+                    "Gen2": "27031",
+                    "lostisland": "27047"
+                },
+                "enemies": [],
+                "tribemates": []
+            }
+        },
+        "Ark Unknown-BG": {
+            "bg": {
+                "server": "Bearded Gamer",
+                "ip": "24.128.67.254",
+                "game": "arkse",
+                "maps": {
+                    "The Island": "27019",
+                    "Aberation": "27023",
+                    "The Center": "27017",
+                    "Ragnorak": "27015",
+                    "Valguero": "27021",
+                    "Crystal Isle": "27033",
+                    "Extinction": "27025",
+                    "Genesis": "27029",
+                    "Gen2": "27031"
+                },
+                "enemies": [],
+                "tribemates": [
+                    "Miso",
+                    "TooMufasa",
+                    "UltraInstinctAzn",
+                    "E10"
+                ]
+            }
+        },
+        "Red Cup Mafia": {
+            "beardedgamers": {
+                "server": "Bearded Gamer",
+                "ip": "24.128.67.254",
+                "game": "arkse",
+                "maps": {
+                    "The Island": "27015",
+                    "Aberation": "27017",
+                    "The Center": "27019",
+                    "Ragnorak": "27021",
+                    "Valguero": "27023",
+                    "Crystal Isle": "27025",
+                    "Extinction": "27027",
+                    "Genesis": "27029",
+                    "Gen2": "27033",
+                    "lostisle": "27031",
+                    "anotherOne": "27047"
+                },
+                "enemies": [],
+                "tribemates": []
+            }
+        },
+        "Ark Alarm Test Server": {
+            "BeardedGamers": {
+                "server": "Bearded Gamer",
+                "ip": "24.128.67.254",
+                "game": "arkse",
+                "maps": {
+                    "The Island": "27019",
+                    "Aberation": "27023",
+                    "The Center": "27017",
+                    "Ragnorak": "27015",
+                    "Valguero": "27021",
+                    "Crystal Isle": "27033",
+                    "Extinction": "27025",
+                    "Genesis": "27029",
+                    "Gen2": "27031",
+                    "lostisland": "27047"
+                },
+                "enemies": [],
+                "tribemates": []
+            }
+        }
+    },
+}
+
+export let server = createServer({
+    routes() {
+        this.get("/:id", (schema, req) => {
+            let params = req.params;
+            let id = params.id;
+            let name = database.keys[id].name;
+            let data = database.serverData[name]
+            //delete database.keys[id];
+            //console.log(database)
+            return {data,name}
+        })
+    }
+})
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
